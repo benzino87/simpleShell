44373df65b7f0f336c5564003aa138f993a5785a
Jason Bensel <benselj@arch08.cis.gvsu.edu>
2017-01-25 16:10:31 -0500

    Working version

--- simpleShell.c
+++ simpleShell.c
@@ -6,9 +6,64 @@
  * *****************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
 
 
 int main(int argc, char *argv[]){
-	printf("hello from shell");
+	//Input from user
+	char input[20];
+
+	pid_t pid, child;
+	int status;
+
+	//Token after splitting user input
+	char *token;
+	
+	//Argument token
+	char *myargs[10];
+
+	//Listen to for user command
+	while(1){
+		printf("bennyshell>");
+		fgets(input, 20, stdin);
+		
+		//Get the first token
+		token = strtok(input, "\t \n");
+		
+		int count = 0;
+		//Get remaining tokens
+		while(token != NULL){
+			printf("%s\n", token);
+			myargs[count] = token;
+			token = strtok(NULL, "\t \n");
+
+			count++;
+		}
+
+		//Check for quit command, exit the shell
+		if(!strcmp(myargs[0], "quit\n")){
+			exit(1);
+		}
+		else if((pid = fork()) < 0){
+			printf("fork failure");
+			exit(1);
+		}
+		else if(pid == 0){
+			printf("Hello from child %ld\n", (long) getpid());
+			myargs[count] = NULL;
+			if(execvp(myargs[0], &myargs[0]) < 0){
+				perror("exec failed");
+			}
+			exit(1);
+		}
+		else {
+			child = waitpid(-1, &status, 0);
+		}
+
+	}
 	return 0;
 }
