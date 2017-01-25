4285994606ab62042d3bb12ab743c99bf04ae53b
Jason Bensel <benselj@arch08.cis.gvsu.edu>
2017-01-25 16:22:55 -0500

    Cleaned up and commented

--- simpleShell.c
+++ simpleShell.c
@@ -13,11 +13,23 @@
 #include <string.h>
 
 
+/*******************************************
+ *
+ * Listens for user input, parses into
+ * input into tokens, spawns a child fork
+ * and executes a system call with specified
+ * tokens. 
+ *
+ * ****************************************/
 int main(int argc, char *argv[]){
+	
 	//Input from user
 	char input[20];
-
+	
+	//Parent pid , child pid	
 	pid_t pid, child;
+	
+	//Status of child fork
 	int status;
 
 	//Token after splitting user input
@@ -26,18 +38,21 @@ int main(int argc, char *argv[]){
 	//Argument token
 	char *myargs[10];
 
-	//Listen to for user command
+	//Handles user input, spawns child fork
+	//executes command
 	while(1){
+		
+		//Request user input
 		printf("bennyshell>");
 		fgets(input, 20, stdin);
 		
 		//Get the first token
 		token = strtok(input, "\t \n");
 		
+
 		int count = 0;
-		//Get remaining tokens
+		//Gets remaining tokens 
 		while(token != NULL){
-			printf("%s\n", token);
 			myargs[count] = token;
 			token = strtok(NULL, "\t \n");
 
@@ -48,21 +63,27 @@ int main(int argc, char *argv[]){
 		if(!strcmp(myargs[0], "quit\n")){
 			exit(1);
 		}
+
+		//Exits on failure to create fork
 		else if((pid = fork()) < 0){
 			printf("fork failure");
 			exit(1);
 		}
+		
+		//Handle standard user input (child)
+		//executes syscall
 		else if(pid == 0){
-			printf("Hello from child %ld\n", (long) getpid());
 			myargs[count] = NULL;
 			if(execvp(myargs[0], &myargs[0]) < 0){
 				perror("exec failed");
 			}
 			exit(1);
 		}
-		else {
-			child = waitpid(-1, &status, 0);
-		}
+		
+		//Parent awaits death of child then continues
+		//iterations of handling user input
+		child = waitpid(-1, &status, 0);
+		printf("Child PID: %ld process ended\n", (long)child);
 
 	}
 	return 0;
